d575cc943c10125677fb9ffb9e8cd954b834fc25
Janek Czapowski <janek.czapowski@comdirect.de>
2020-07-30 08:44:44 +0200

    added a player switch in board engine

--- app/src/main/java/com/example/tictactoeadfree/gameEngine/TicTacToeEngine.kt
+++ app/src/main/java/com/example/tictactoeadfree/gameEngine/TicTacToeEngine.kt
@@ -1,15 +1,15 @@
 package com.example.tictactoeadfree.gameEngine
 
-import java.lang.IllegalArgumentException
-
 class TicTacToeEngine constructor(
-    val grid: Int = 3,
-    val winCondition: Int = 3,
+    private val grid: Int = 3,
+    private val winCondition: Int = 3,
     private val is3DBoard: Boolean = false
 ) {
-    private var playGround = mutableListOf<MutableList<MutableList<Any?>>>()
+    private var playGround = mutableListOf<MutableList<MutableList<Int?>>>()
 
-    private var currentPlayer = 1
+    private var currentPlayer = 0
+
+    private val rowAmountToWin = if (grid == 3) { 3 } else { 4 }
 
     //TODO in späteren Updates eventuell den Nutzer entscheiden lassen wieviele Spieler es sein sollen
     private val playerCount = 2
@@ -24,11 +24,17 @@ class TicTacToeEngine constructor(
         }
         playGround[positionX][positionY][positionZ] = currentPlayer
 
+        if (currentPlayer == playerCount - 1) {
+            currentPlayer = 0
+        } else {
+            currentPlayer++
+        }
+
         checkForWinCondition()
     }
 
     private fun checkForWinCondition() {
-        TODO("Not yet implemented")
+        //TODO("Not yet implemented")
     }
 
     interface EndedGameListener {
