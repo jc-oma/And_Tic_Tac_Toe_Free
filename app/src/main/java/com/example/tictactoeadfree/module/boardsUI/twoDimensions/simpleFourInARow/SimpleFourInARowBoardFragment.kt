5ddcd69fa815d44322bbc94842c816f7dce5854e
jc093 <jc093@uni-rostock.de>
2020-08-19 14:44:49 +0200

    a lot of constraints tried to fix four in a row now ready to implement

--- app/src/main/java/com/example/tictactoeadfree/module/boardsUI/twoDimensions/simpleFourInARow/SimpleFourInARowBoardFragment.kt
+++ app/src/main/java/com/example/tictactoeadfree/module/boardsUI/twoDimensions/simpleFourInARow/SimpleFourInARowBoardFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 import com.example.tictactoeadfree.R
+import kotlinx.android.synthetic.main.fragment_simple_four_in_a_row_board.*
 
 class SimpleFourInARowBoardFragment : Fragment() {
     companion object {
@@ -19,5 +21,20 @@ class SimpleFourInARowBoardFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_simple_four_in_a_row_board, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        moveBoard()
+    }
+
+    private fun moveBoard() {
+        val fallDownAnimation = AnimationUtils.loadAnimation(
+            context,
+            R.anim.move_for_in_a_row_a_bit
+        )
+
+        fragment_four_in_a_row_playboard.startAnimation(fallDownAnimation)
     }
 }
