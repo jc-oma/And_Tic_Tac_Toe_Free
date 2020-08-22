3714804c21de0916c85fc42ff9eb5796d050b66b
Janek Czapowski <janek.czapowski@comdirect.de>
2020-08-22 15:16:34 +0200

    added a throttle to ignore restart clicks for 5 seconds in FIAR Board

--- app/src/main/java/com/example/tictactoeadfree/module/boardsUI/twoDimensions/simpleFourInARow/SimpleFourInARowBoardFragment.kt
+++ app/src/main/java/com/example/tictactoeadfree/module/boardsUI/twoDimensions/simpleFourInARow/SimpleFourInARowBoardFragment.kt
@@ -2,15 +2,16 @@ package com.example.tictactoeadfree.module.boardsUI.twoDimensions.simpleFourInAR
 
 import android.os.Bundle
 import android.os.Handler
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.animation.AnimationUtils
 import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
 import com.example.tictactoeadfree.R
 import com.example.tictactoeadfree.module.data.gameSettings.GameMode
 import com.example.tictactoeadfree.module.statistics.StatisticsUtils
+import com.jakewharton.rxbinding4.view.clicks
 import kotlinx.android.synthetic.main.fragment_simple_four_in_a_row_board.*
 
 class SimpleFourInARowBoardFragment : Fragment() {
@@ -35,7 +36,11 @@ class SimpleFourInARowBoardFragment : Fragment() {
         fragment_four_in_a_row_playboard.appEventFlowable.subscribe {
             Handler().postDelayed({
                 four_in_a_row_game_end_overlay.isVisible = true
-                four_in_a_row_game_end_overlay.onGameWon(it.wonPlayer, StatisticsUtils(context).getDrawablesPair(GameMode.FOUR_IN_A_ROW), GameMode.FOUR_IN_A_ROW)
+                four_in_a_row_game_end_overlay.onGameWon(
+                    it.wonPlayer,
+                    StatisticsUtils(context).getDrawablesPair(GameMode.FOUR_IN_A_ROW),
+                    GameMode.FOUR_IN_A_ROW
+                )
                 whobbleRestartButton(true)
             }, 1200)
         }
@@ -60,9 +65,11 @@ class SimpleFourInARowBoardFragment : Fragment() {
             four_in_a_row_game_end_overlay.isVisible = false
         }
 
-        four_in_a_row_button_text.setOnClickListener {
-            fragment_four_in_a_row_playboard.restartBoard()
-            whobbleRestartButton(false)
-        }
+        val throttleDuration: Long = 5000
+        four_in_a_row_button_text.clicks()
+            .throttleFirst(throttleDuration, java.util.concurrent.TimeUnit.MILLISECONDS).subscribe {
+                fragment_four_in_a_row_playboard.restartBoard()
+                whobbleRestartButton(false)
+            }
     }
 }
