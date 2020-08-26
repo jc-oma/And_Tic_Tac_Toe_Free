ee2da7e772619f9d455656b00f3c90fee7be701c
jc093 <jc093@uni-rostock.de>
2020-08-26 12:15:07 +0200

    added a fragment to show external used libraries in this connection a recycler view was added to adapt them

--- app/src/main/java/com/jcDevelopment/tictactoeadfree/module/usedLibraries/UsedLibrariesList.kt
+++ app/src/main/java/com/jcDevelopment/tictactoeadfree/module/usedLibraries/UsedLibrariesList.kt
@@ -8,7 +8,10 @@ class UsedLibrariesList (context: Context?) {
     fun getUsedLibraries() :List<LibraryDataClass>{
         return if (context != null)
             listOf(
-                LibraryDataClass(context.getString(R.string.koin_license_headline),context.getString(R.string.koin_license_text))
+                LibraryDataClass(context.getString(R.string.license_koin_headline),context.getString(R.string.license_koin_license_text)),
+                LibraryDataClass(context.getString(R.string.license_konfetti_license_headline),context.getString(R.string.license_konfetti_license_text)),
+                LibraryDataClass(context.getString(R.string.license_rxAndroid_headline),context.getString(R.string.license_rxandroid_license_text)),
+                LibraryDataClass(context.getString(R.string.license_rxBindings_headline),context.getString(R.string.license_rxbindings_license_text))
             )
         else emptyList()
     }
