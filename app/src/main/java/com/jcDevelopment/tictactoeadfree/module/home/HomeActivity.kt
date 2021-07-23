c2cf7ed158069d9e466efb55c63569270938a9f0
jc093 <jc093@uni-rostock.de>
2021-07-23 09:08:47 +0200

    fixed ads issue which led to not showing ad because not initialised on second show

--- app/src/main/java/com/jcDevelopment/tictactoeadfree/module/home/HomeActivity.kt
+++ app/src/main/java/com/jcDevelopment/tictactoeadfree/module/home/HomeActivity.kt
@@ -642,6 +642,7 @@ class HomeActivity : BaseActivity(), HomeFragment.Listener, CompanyLogoFragment.
     private fun showInterstitialAd() {
         if (mInterstitialAd != null) {
             mInterstitialAd?.show(this)
+            initAds()
         } else {
             Log.d("TAG", "The interstitial ad wasn't ready yet.")
         }
